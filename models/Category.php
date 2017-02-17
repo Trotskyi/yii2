50b38eb0e59b2b39b5edb1a3d58db07a913ac102
Trotskyi Oleksandr <trotskyioleksandr@gmail.com>
2017-02-17 23:44:06 +0200

    create functional for redirect article in templates our sites

--- models/Category.php
+++ models/Category.php
@@ -4,6 +4,7 @@ namespace app\models;
 
 use app\modules\admin\controllers\ArticleController;
 use Yii;
+use yii\data\Pagination;
 
 /**
  * This is the model class for table "category".
@@ -19,6 +20,11 @@ class Category extends \yii\db\ActiveRecord
     public static function tableName()
     {
         return 'category';
+    }
+
+    public static function getAll()
+    {
+      return  Category::find()->all();
     }
 
     /**
@@ -45,5 +51,35 @@ class Category extends \yii\db\ActiveRecord
     public function getArticles()
     {
         return $this->hasMany(Article::className(), ['category_id' => 'id']);
+    }
+
+    public function getArticlesCount()
+    {
+        return $this->getArticles()->count();
+    }
+
+
+    public static function getArticlesByCategory($id)
+    {
+
+        // build a DB query to get all articles with status = 1
+        $query = Article::find()->where(['category_id'=>$id]);
+
+        // get the total number of articles (but do not fetch the article data yet)
+        $count = $query->count();
+
+        // create a pagination object with the total count
+        $pagination = new Pagination(['totalCount' => $count, 'pageSize' => 6]);
+
+        // limit the query using the pagination and retrieve the articles
+        $articles = $query->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+
+
+        $data['articles'] = $articles;
+        $data['pagination'] = $pagination;
+
+        return $data;
     }
 }
